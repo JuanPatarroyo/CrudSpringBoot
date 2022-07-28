5c6539e605c2e5b85281eeaa4052435b1b9b7f41
Juan Patarroyo Ardila <juanestebanpatarroyo@gmail.com>
2022-07-27 23:55:42 -0500

    New users are added from configuration file

---
+++ src/main/java/co/com/web/SecurityConfig.java
@@ -0,0 +1,21 @@
+package co.com.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author jpatarroyo
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin123").roles("ADMIN", "USER")
+                .and().withUser("juanes").password("{noop}juanes123").roles("USER");
+    }
+}
